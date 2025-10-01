43f0dc4dfb244fa734ac5b0d87306ab88ef83001
Alexander Kuhn <akuhn.bmw@gmail.com>
2025-10-01 13:42:17 +0000

    created all the models

---
+++ models/mart/silverscreen_monthly_revenue.sql
@@ -0,0 +1,15 @@
+select
+    i.movie_id, 
+    m.movie_title, 
+    m.genre, 
+    i.studio,
+    i.month,
+    i.location_id,
+    i.total_invoice_sum as rental_cost,
+    s.total_tickets as tickets_sold,
+    s.total_revenue as revenue 
+from {{ ref('stg_invoices') }} i 
+left join {{ ref('stg_sales') }} s 
+on i.movie_id = s.movie_id and i.month = s.transaction_month and i.location_id = s.location_id
+left join {{ ref('stg_public__movie_catalouge') }} m 
+on i.movie_id = m.movie_id 
